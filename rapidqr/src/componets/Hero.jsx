a771f9e3a8230dceccc43a4a1b90292287709dc2
akash <akashak1602@gmail.com>
2023-12-04 12:49:08 +0530

    download option added

--- rapidqr/src/componets/Hero.jsx
+++ rapidqr/src/componets/Hero.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import * as htmlToImage from 'html-to-image';
 
 function Hero() {
     const [inputValue, setInputValue] = useState('');
+    const [showQRCode, setShowQRCode] = useState(false);
 
     useEffect(() => {
         const script = document.createElement('script');
@@ -22,63 +24,88 @@ function Hero() {
             qrcodeContainer.innerHTML = "";
             new window.QRCode(qrcodeContainer, website);
             document.getElementById("qrcode-container").style.display = "block";
+            setShowQRCode(true);
         } else {
             alert("Please enter a valid URL");
         }
     };
 
+    const downloadQRCode = () => {
+        const qrcodeContainer = document.getElementById("qrcode-container");
+
+        htmlToImage.toJpeg(qrcodeContainer)
+            .then(function (dataUrl) {
+                const link = document.createElement("a");
+                link.href = dataUrl;
+                link.download = "qrcode.jpg";
+
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            });
+    };
+
     return (
         <>
-        <div className='bg-gradient-to-r from-indigo-400 from-10% via-sky-500 via-30% to-emerald-300 to-90% ... md:h-screen h-screen'>
+            <div className='bg-gradient-to-r from-indigo-400 from-10% via-sky-500 via-30% to-emerald-300 to-90% ... md:h-screen h-screen'>
 
-            <div className="flex flex-col mb-6 pt-6 ">
-                <div className="w-full flex justify-center items-center">
-                    <h1 className='md:text-3xl text-xl font-Poppins text-white'>Welcome</h1>
-                </div>
-                <div className="w-full flex justify-center items-center">
-                    <h1 className='md:text-7xl text-5xl font-Poppins tracking-wider text-white'>Rapid Qr</h1>
-                </div>
-            </div>
-
-            <div className="form flex flex-col items-center">
-                <h1 className="mb-4 text-2xl font-Poppins text-white">Please enter your URL</h1>
-                <form className="flex-col flex items-center justify-center">
-                    <div className="mb-4 border rounded-md p-2 focus-within:border-gray-500">
-                        <input
-                            type="url"
-                            id="website"
-                            name="website"
-                            placeholder="https://site.com"
-                            value={inputValue}
-                            onChange={(e) => setInputValue(e.target.value)}
-                            required
-                            className="w-full outline-none"
-                        />
+                <div className="flex flex-col mb-6 pt-6 ">
+                    <div className="w-full flex justify-center items-center">
+                        <h1 className='md:text-3xl text-xl font-Poppins text-white'>Welcome</h1>
                     </div>
+                    <div className="w-full flex justify-center items-center">
+                        <h1 className='md:text-7xl text-5xl font-Poppins tracking-wider text-white'>Rapid Qr</h1>
+                    </div>
+                </div>
 
-                    {inputValue && !inputValue.includes('.com') && !inputValue.includes('.app') && (
-                        <div className="mb-4 justify-start items-start">
-                            <p className='text-red-500'>https://site.com</p>
+                <div className="form flex flex-col items-center">
+                    <h1 className="mb-4 text-2xl font-Poppins text-white">Please enter your URL</h1>
+                    <form className="flex-col flex items-center justify-center">
+                        <div className="mb-4 border rounded-md p-2 focus-within:border-gray-500">
+                            <input
+                                type="url"
+                                id="website"
+                                name="website"
+                                placeholder="https://site.com"
+                                value={inputValue}
+                                onChange={(e) => setInputValue(e.target.value)}
+                                required
+                                className="w-full outline-none"
+                            />
                         </div>
-                    )}
 
-                    {inputValue && 
-                        <button
-                            type="button"
-                            onClick={generateQRCode}
-                            className="bg-blue-500 text-white px-4 py-2 rounded mb-4"
-                        >
-                            Generate QR Code
-                        </button>
-                    }
+                        {inputValue && !inputValue.includes('.com') && !inputValue.includes('.app') && (
+                            <div className="mb-4 justify-start items-start">
+                                <p className='text-red-500'>https://site.com</p>
+                            </div>
+                        )}
 
-                    <div id="qrcode-container" className="mb-4">
-                        <div id="qrcode" className="qrcode"></div>
-                    </div>
-                </form>
+                        {inputValue && 
+                            <button
+                                type="button"
+                                onClick={generateQRCode}
+                                className="bg-blue-500 text-white px-4 py-2 rounded mb-4"
+                            >
+                                Generate QR Code
+                            </button>
+                        }
+
+                        <div id="qrcode-container" className="mb-4">
+                            <div id="qrcode" className="qrcode"></div>
+                        </div>
+
+                        {showQRCode &&
+                            <button
+                                type="button"
+                                onClick={downloadQRCode}
+                                className="bg-green-500 text-white px-4 py-2 rounded"
+                            >
+                                Download QR Code (JPG)
+                            </button>
+                        }
+                    </form>
+                </div>
             </div>
-            </div>
-
         </>
     );
 }
